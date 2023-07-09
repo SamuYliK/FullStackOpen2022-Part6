2fa7369e318e63cd5f8a7f140d7404aea5592fa3
SamuYliK <samu.ylikuivila@gmail.com>
2023-07-09 18:35:33 +0300

    Intermediate commit, includes exercises 6.1 - 6.9

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -3,7 +3,8 @@ import { voteAnecdote } from '../reducers/anecdoteReducer'
 
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => state.anecdotes)
+    const filter = useSelector(state => state.filter)
     const dispatch = useDispatch()
 
     const vote = (id) => {
@@ -12,7 +13,11 @@ const AnecdoteList = () => {
 
     return (
         <div>
-        {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
+        {anecdotes
+        .filter(el => 
+            el.content.toLowerCase().includes(filter.toLowerCase()))
+        .sort((a, b) => b.votes - a.votes)
+        .map(anecdote =>
             <div key={anecdote.id}>
             <div>
                 {anecdote.content}
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -33,7 +33,7 @@ export const addNewAnecdote = (content) => {
   }
 }
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
   switch(action.type){
@@ -55,4 +55,4 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-export default reducer
+export default anecdoteReducer
